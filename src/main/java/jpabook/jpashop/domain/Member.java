6a56f35013e1bef2de17a29a8523eb9af4cb78c1
POIUY <bumcheol1@gmail.com>
2026-02-28 18:42:29 +0900

    엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // member에 매핑됨 - 난 거울이(읽기전용) -> FK가 변경되지 않음.
+    private List<Order> orders = new ArrayList<>();
+}
